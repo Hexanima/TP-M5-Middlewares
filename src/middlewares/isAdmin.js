5654b8e0c5e5ead230d5e46269239bcd17265f72
Hexanima <nicolasfilippelli22@gmail.com>
2023-02-20 14:59:22 -0300

    Arreglado isAdmin, solucionado error 'send' despues de 'next'

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,14 +1,12 @@
 const admins = ["Ada", "Greta", "Vim", "Tim"];
 
-function isAdmin (req, res, next) {
-    let user = req.query.user;
-    admins.forEach(admin => {
-        if (user == admin) {
-            next();
-        }
-    })
-
+function isAdmin(req, res, next) {
+  let user = req.query.user;
+  if (admins.includes(user)) {
+    next();
+  } else {
     res.send("No tienes los privilegios para ingresar");
+  }
 }
 
 module.exports = isAdmin;
